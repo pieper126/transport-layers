8a2e3bfb4fa60746d3c3e304c2db8ef2db8de7e5
Stijn Bijnen <stijnbijnen@hotmail.com>
2025-02-09 16:52:08 +0100

    Add socket call in go client

--- client-go/main.go
+++ client-go/main.go
@@ -3,16 +3,73 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
+	"os/signal"
 	"strings"
 	"sync"
+	"time"
+
+	"github.com/gorilla/websocket"
 )
 
+const amount = 1000
+const message = `{ "json": "test" }`
+
 func main() {
+	fmt.Println("starting websocket!")
+	callingViaWebsocket()
+}
+
+func callingViaWebsocket() {
+	conn, _, err := websocket.DefaultDialer.Dial("ws://localhost:3000/ws/echo/", nil)
+	// conn, _, err := websocket.DefaultDialer.Dial("wss://echo.websocket.org/", nil)
+	if err != nil {
+		fmt.Printf("err occurred: %e", err)
+		return
+	}
+	defer conn.Close()
+
+	err = conn.WriteMessage(websocket.TextMessage, []byte(message))
+	if err != nil {
+		fmt.Printf("err occurred: %e", err)
+		return
+	}
+
+	interupt := make(chan os.Signal, 1)
+	signal.Notify(interupt, os.Interrupt)
+
+	go func() {
+		for {
+			_, msg, err := conn.ReadMessage()
+			if err != nil {
+				fmt.Printf("err occurred: %e", err)
+				return
+			}
+			fmt.Println("Received: ", string(msg))
+
+			if string(msg) == string(message) {
+				interupt <- os.Interrupt
+			}
+		}
+	}()
+
+	for {
+		select {
+		case <-interupt:
+			fmt.Println("closing connection...")
+			conn.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, "bye!"))
+			time.Sleep(time.Millisecond * 100)
+			return
+		}
+	}
+
+}
+
+func callingViaNormallHttp() {
 	client := http.Client{}
 
 	wg := sync.WaitGroup{}
 
-	amount := 100_000
 	wg.Add(amount)
 
 	for i := 0; i < amount; i++ {
@@ -30,7 +87,8 @@ func callServer(client *http.Client) {
 
 	reader := strings.NewReader(bodyString)
 
-	res, err := client.Post("http://localhost:3000/echo/", "application/json", reader)
+	res, err := client.Post("http://localhost:3000/ws/echo/", "application/json", reader)
+	// res, err := client.Post("http://localhost:3000/echo/", "application/json", reader)
 	if err != nil {
 		fmt.Printf("err %e", err)
 	}
@@ -39,4 +97,6 @@ func callServer(client *http.Client) {
 
 	res_body := make([]byte, 256)
 	res.Body.Read(res_body)
+
+	fmt.Printf("res: %s", string(res_body))
 }
